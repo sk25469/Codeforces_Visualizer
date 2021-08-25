900dfc0b9e60a59406594404c58355bea05a78f8
sk25469 <sk25469@gmail.com>
2021-08-25 16:43:04 +0530

    some ui changes

--- lib/widgets/contest_details.dart
+++ lib/widgets/contest_details.dart
@@ -14,22 +14,57 @@ class ContestDetail extends StatelessWidget {
       ),
       child: Card(
         elevation: 5,
-        child: ListTile(
+        child: InkWell(
           onTap: () {},
-          contentPadding: const EdgeInsets.all(5),
-          leading: Column(
-            // ignore: prefer_const_literals_to_create_immutables
-            children: [
-              const Text('Start Time '),
-              const SizedBox(height: 8),
-              const Text('Aug 26'),
-            ],
-          ),
-          title: const Text('Codeforces Round #767'),
-          trailing: IconButton(
-            tooltip: 'Register for the contest',
-            onPressed: () => {},
-            icon: const Icon(Icons.app_registration_rounded),
+          child: Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    const Text(
+                      'Start Time',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
+                    const SizedBox(height: 3.5),
+                    const Text('Aug 30'),
+                  ],
+                ),
+                Column(
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    const Text(
+                      'Contest Name',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
+                    const SizedBox(height: 3.5),
+                    const Text('Codeforces Round #756'),
+                  ],
+                ),
+                Column(
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    const Text(
+                      'Duration',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
+                    const SizedBox(height: 3.5),
+                    const Text('2:15'),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
