88d0cf2116cb76d1ca2349ac46d3fb88ab535b06
sk25469 <sk25469@gmail.com>
2021-08-27 00:39:46 +0530

    changed tabs bar to bottom navigation bar

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,6 +1,7 @@
 import 'package:codeforces_visualizer/screens/contest_screen.dart';
 import 'package:codeforces_visualizer/screens/user_info_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:titled_navigation_bar/titled_navigation_bar.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -8,37 +9,45 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': const ContestScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': const UserInfoScreen(),
+      'title': 'Your Favorite',
+    },
+  ];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      // initialIndex: 0,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Codeforces Visualizer'),
-          bottom: const TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(
-                  Icons.calendar_view_month,
-                ),
-                text: 'Contests',
-              ),
-              Tab(
-                icon: Icon(
-                  Icons.query_stats,
-                ),
-                text: 'Stats',
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title'] as String),
+      ),
+      body: _pages[_selectedPageIndex]['page'] as Widget,
+      bottomNavigationBar: TitledBottomNavigationBar(
+        currentIndex: _selectedPageIndex,
+        onTap: _selectPage,
+        reverse: true,
+        items: [
+          TitledNavigationBarItem(
+            title: const Text('Contests'),
+            icon: const Icon(Icons.calendar_today),
           ),
-        ),
-        body: const TabBarView(
-          children: <Widget>[
-            ContestScreen(),
-            UserInfoScreen(),
-          ],
-        ),
+          TitledNavigationBarItem(
+            title: const Text('Stats'),
+            icon: const Icon(Icons.query_stats),
+          ),
+        ],
       ),
     );
   }
